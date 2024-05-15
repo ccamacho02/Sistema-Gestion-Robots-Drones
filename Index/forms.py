d3241bce23bfd8ba355ef1f1adbb2a65750a0ca1
FELIPE8976 <lufema2003@gmail.com>
2024-05-15 16:21:19 -0500

    Se agrego la funcion de inicio sesion y se creo el sidebar

---
+++ Index/forms.py
@@ -0,0 +1,7 @@
+from django.contrib.auth.forms import AuthenticationForm
+
+class CustomAuthenticationForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].label = 'Nombre de usuario'
+        self.fields['password'].label = 'Contraseña'
