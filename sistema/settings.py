3ab1af83e9bf64fba6a4d8879952c8d7f8ff17b9
Carlos Camacho <carloscamacho0602@gmail.com>
2024-05-15 16:36:22 -0500

    Se agrega funcionalidad de envio de correo electronico a usuario y admin

--- sistema/settings.py
+++ sistema/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Index',
+    'gestion',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'sistema.gestion.drones@gmail.com'
+EMAIL_HOST_PASSWORD = 'mwvjygbipcpmmhan'
+EMAIL_USE_TLS = True
